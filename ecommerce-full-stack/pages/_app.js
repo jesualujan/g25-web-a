7ef3d38a2d898f02d991b5fe2e397b8d20af7102
jesualujan <lujan.oficial@gmail.com>
2024-01-24 21:07:17 -0600

    ✨ importamos los estilos de Chakra-UI

--- ecommerce-full-stack/pages/_app.js
+++ ecommerce-full-stack/pages/_app.js
@@ -1,5 +1,11 @@
+//* IMPORTAR LOS ESTILOS NECESARIOS DE CHAKRA-UI
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<ChakraProvider>
+    <Component {...pageProps} />
+</ChakraProvider>
+  )   
 }
