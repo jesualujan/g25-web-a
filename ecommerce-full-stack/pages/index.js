a2906990fcb007a31e31c001376d543d912cd3cd
jesualujan <lujan.oficial@gmail.com>
2024-01-24 21:39:23 -0600

    ✨ Importamos el componente Products

--- ecommerce-full-stack/pages/index.js
+++ ecommerce-full-stack/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout"
+import { Image } from "@chakra-ui/react"
+import Products from "../components/Products"
 
 export default function Home() {
   return (
     <>
       <Layout>
-        
+        <Products/>
       </Layout>
     </>
   )
