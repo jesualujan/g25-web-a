73dd348ab5c8fbdde29e8052256242ab1324e367
jesualujan <lujan.oficial@gmail.com>
2024-01-24 21:38:39 -0600

    ✨ creamos la data de products

---
+++ ecommerce-full-stack/utils/data.js
@@ -0,0 +1,61 @@
+// data dummy for products 
+export const data = {
+    products: [
+        {
+            id:1,
+            title: 'Produc 1',
+            description: 'Product 1 description',
+            price: 100,
+            image: '/23.jpg'
+        },
+        {
+            id:2,
+            title: 'Produc 2',
+            description: 'Product 2 description',
+            price: 200,
+            image: '/46.jpg'
+        },
+        {
+            id:3,
+            title: 'Produc 3',
+            description: 'Product 3 description',
+            price: 300,
+            image: '/72.jpg'
+        },
+        {
+            id:4,
+            title: 'Produc 4',
+            description: 'Product 4 description',
+            price: 400,
+            image: '/73.jpg'
+        },
+        {
+            id:5,
+            title: 'Produc 5',
+            description: 'Product 5 description',
+            price: 500,
+            image: '/74.jpg'
+        },
+        {
+            id:6,
+            title: 'Produc 6',
+            description: 'Product 6 description',
+            price: 600,
+            image: '/75.jpg'
+        },
+        {
+            id:7,
+            title: 'Produc 7',
+            description: 'Product 7 description',
+            price: 700,
+            image: '/76.jpg'
+        },
+        {
+            id:8,
+            title: 'Produc 8',
+            description: 'Product 8 description',
+            price: 800,
+            image: '/75.jpg'
+        },
+    ]
+}
