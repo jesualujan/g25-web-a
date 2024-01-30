15cc15553be950b65c05649ccfb7844a71a4e589
jesualujan <lujan.oficial@gmail.com>
2024-01-29 19:34:54 -0600

    ✨ Quitamos el componente Layout

--- ecommerce-full-stack/pages/index.js
+++ ecommerce-full-stack/pages/index.js
@@ -1,13 +1,10 @@
-import Layout from "../components/Layout"
 import { Image } from "@chakra-ui/react"
 import Products from "../components/Products"
 
 export default function Home() {
   return (
     <>
-      <Layout>
         <Products/>
-      </Layout>
     </>
   )
 }
