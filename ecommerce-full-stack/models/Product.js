48de02c5bd19b23fd7b05fa520b573494fa9ddf9
jesualujan <lujan.oficial@gmail.com>
2024-01-29 20:58:59 -0600

    ✨ creamos el schema de product

---
+++ ecommerce-full-stack/models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema(
+    { 
+        id: {type: String, required: true, unique: true},
+        title: {type: String, required: true},
+        image: {type: String, required: true},
+        price: {type: Number, required: true},
+        description: {type: String, required: true},
+    },
+    { timestamps: true } // sirve para saber cuando un producto (dato) fue creado
+)
+
+const Product = mongoose.model.Product || mongoose.model('Product', productSchema)
+export default Product
