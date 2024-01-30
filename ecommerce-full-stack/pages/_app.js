c8cfef53bb1c626b0dfd5c813310e8abd388b5a5
jesualujan <lujan.oficial@gmail.com>
2024-01-29 19:34:38 -0600

    ✨ importamos nuestro componente Layout

--- ecommerce-full-stack/pages/_app.js
+++ ecommerce-full-stack/pages/_app.js
@@ -1,11 +1,13 @@
 //* IMPORTAR LOS ESTILOS NECESARIOS DE CHAKRA-UI
 import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
+import Layout from "../components/Layout"
 
 export default function App({ Component, pageProps }) {
   return (
 <ChakraProvider>
-    <Component {...pageProps} />
+  <Layout>
+  <Component {...pageProps} />
+  </Layout>
 </ChakraProvider>
   )   
 }
