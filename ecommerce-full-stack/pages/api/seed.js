8219f56e44d6b2ee806ce4c777a789ff770b13eb
jesualujan <lujan.oficial@gmail.com>
2024-01-31 14:37:17 -0600

    ✨ importe el nuevo archivo js Products

--- ecommerce-full-stack/pages/api/seed.js
+++ ecommerce-full-stack/pages/api/seed.js
@@ -1,26 +1,27 @@
 import { createRouter } from 'next-connect'
 import db from '../../utils/db'
-import  { data } from '../../utils/data'
-import Product from '../../models/Product'
+import {data} from '../../utils/data'
+import Product from '../../models/Products';
 
-const router = createRouter()
+const router = createRouter();
 
 router
   .get(async (req, res) => {
-     //? MONGOOSE FUNCTIONS 
-     await db.connect()
-     await Product.deleteMany()
-     await Product.insertMany(data.products)
-     await db.disconnect()
-    res.send('IT WORKS')
+    //? MONGOOSE FUNCTIONS 
+    await db.connect()
+    await Product.deleteMany()
+    await Product.insertMany(data.products)
+    await db.disconnect()
+    res.send("IT WORKS")
   })
+
 
 export default router.handler({
   onError: (err, req, res) => {
     console.error(err.stack);
     res.status(err.statusCode || 500).end(err.message);
   },
-  onNoMatch: (req,res) =>{
-    res.status(404).end('Page in not found')
+  onNoMatch: (req,res) => {
+    res.status(404).end('Page is not found');
   }
 });
