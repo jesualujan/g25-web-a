b3c3b7c3efcf1384cc93d122229769ad550a2f1d
jesualujan <lujan.oficial@gmail.com>
2024-01-31 14:36:49 -0600

    ✨elimine el import de Image, idente el código

--- ecommerce-full-stack/pages/index.js
+++ ecommerce-full-stack/pages/index.js
@@ -1,10 +1,41 @@
-import { Image } from "@chakra-ui/react"
-import Products from "../components/Products"
+import Products from '../components/Products'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    fetchProduct()
+  },[])
+
+  async function fetchProduct(){
+      try {
+        setLoading(true)
+        setError(false)
+        const res = await fetch('/api/products')
+        const newProducts = await res.json()
+        setProducts(newProducts)
+      }catch (err) {
+        setError(true)
+      }
+      setLoading(false)
+  }
+
+  
+  if(error) {
+    return <div>Error al cargar...</div>
+  }
+
+  if(loading) {
+    return <div>Cargando...</div>
+  }
+
+
   return (
     <>
-        <Products/>
+      <Products data={products}/>
     </>
   )
 }
