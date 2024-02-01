3c8dd8e5a30f4968c595112fe0dfe9b47f1b99a7
jesualujan <lujan.oficial@gmail.com>
2024-01-31 19:44:01 -0600

    ✨ creo la función para manejar el contexto

--- ecommerce-full-stack/context/CartContext.js
+++ ecommerce-full-stack/context/CartContext.js
@@ -4,6 +4,24 @@ const initialState = {
     cart: []
 }
 
+const cartReducer = (state, action) => {
+    switch (action.type){
+        case "ADD_TO_CART":
+            return {
+                ...state,
+                cart: [...state.cart, action.payload]
+            }
+        case "REMOVE_FROM_CART":
+            return {
+                ...state,
+                cart: state.cart.filter(item => item._id !==  action.payload)
+            }
+            default: 
+                return state;
+    }
+}
+
+
 const CartContext = createContext()
     
 const CartProvider = ({ children}) => {
