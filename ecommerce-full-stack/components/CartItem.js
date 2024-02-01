d7a07f297f08e5dce960fc2dc77e40df01afece8
jesualujan <lujan.oficial@gmail.com>
2024-01-31 22:09:00 -0600

    ✨ creamos el componente con el nombre indicado CartItem

---
+++ ecommerce-full-stack/components/CartItem.js
@@ -0,0 +1,36 @@
+import React, {useContext} from 'react'
+import {
+    Box,
+    Flex,
+    Stack,
+    Text,
+    Image,
+    useColorModeValue,
+    CloseButton
+} from '@chakra-ui/react'
+import { CartContext } from '../context/CartContext'
+
+const CartItem = ({ item }) => {
+    const { removeFromCart } = useContext(CartContext)
+  return (
+    <Flex direction={{base: 'column', md:'row'}} justify="space-between" align="center">
+        <Stack direction={'row'} spacing={5} width="full">
+            <Image src={`/images${item.image}`} alt={item.title} w={'100%'} h={'100%'} loading='lazy'/>
+            <Box pt="4">
+                <Stack>
+                    <Text fontWeight="medium">{item.title}</Text>
+                    <Text fontSize="sm" color={useColorModeValue('gray.500', 'gray.600')}>{item.description}</Text>
+                </Stack>
+            </Box>
+        </Stack>
+        <Flex width="full" justify="space-between" display="flex">
+            <Text fontWeight={'medium'} fontSize='lg' color={useColorModeValue('gray.700', 'gray.200')}>
+              ${item.price}.00
+            </Text>
+            <CloseButton size="md" onClick={() => removeFromCart(item.id)}/>
+        </Flex>
+    </Flex>
+  )
+}
+
+export default CartItem
