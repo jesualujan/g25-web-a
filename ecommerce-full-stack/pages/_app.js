0a47b8a01001f810da7f68f98446864e5d05cad7
jesualujan <lujan.oficial@gmail.com>
2024-01-31 19:36:35 -0600

    ✨ Pasamos el contexto

--- ecommerce-full-stack/pages/_app.js
+++ ecommerce-full-stack/pages/_app.js
@@ -1,13 +1,16 @@
 //* IMPORTAR LOS ESTILOS NECESARIOS DE CHAKRA-UI
-import { ChakraProvider } from '@chakra-ui/react'
-import Layout from "../components/Layout"
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+import { CartProvider } from "../context/CartContext";
 
 export default function App({ Component, pageProps }) {
   return (
-<ChakraProvider>
-  <Layout>
-  <Component {...pageProps} />
-  </Layout>
-</ChakraProvider>
-  )   
+    <ChakraProvider>
+      <CartProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartProvider>
+    </ChakraProvider>
+  );
 }
