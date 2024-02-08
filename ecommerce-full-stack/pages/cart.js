b556bd6458c9ad7c04cd5098d1634549f8ef72b8
jesualujan <lujan.oficial@gmail.com>
2024-02-07 19:42:29 -0600

    ✨ Validamos y calculamos el total

--- ecommerce-full-stack/pages/cart.js
+++ ecommerce-full-stack/pages/cart.js
@@ -8,26 +8,57 @@ import {
     Heading,
     Flex
     } from '@chakra-ui/react'
+import OrderSummary from '../components/OrderSummary'
 
 
 const CartPage = () => {
     const {cart} = useContext(CartContext)
 
+    const calculateTotal = () => {
+      return cartItem.reduce((acc,item) => acc + item.price, 0)
+    }
+
   return (
-     <Box>
-        <Text>
-            Your cart is empty
-        </Text>
-            <>
-                <Stack>
-                    <Stack>
-                        <Heading as="h1" size='2xl'>Your Shopping Bag</Heading>
-                        <Stack>
-                            <CartItem />
+     <Box
+        maxW={{base:'3xl', lg:'5xl'}}
+        mx="auto"
+        px={{base: '4', md: '8', lg: '12'}}
+        py={{base: '4', md: '8', lg: '12'}}
+     >
+            {/* validación if ? else :  */}
+
+            {
+               cart?.length === 0 ?
+               (
+                <Text fontSize='xl' fontWeight='bold'> Your Cart is empty </Text>
+               ) :
+               (
+                <>
+                <Stack
+                as="section"
+                spacing={{base: '9', lg:'14'}}
+                direction={{base: 'column', lg: 'row'}}
+                align={{lg: 'flex-start'}}
+                >
+
+                <Stack flex="2" spacing={{base: 6, lg:'10'}}>
+                    <Heading as="h1" size="2xl">Shopping Cart</Heading>
+                    <Stack spacing={'6'}>
+                        {
+                            cart?.length > 0 && cart.map((item) => (
+                                <CartItem key={item.id} item={item} />
+                            ))
+                        }
                         </Stack>
                     </Stack>
+
+                        <Flex direction="column" align="center" flex="1">
+                            <OrderSummary />
+                        </Flex>
                 </Stack>
             </>
+               )
+            }
      </Box>
   )
 }
