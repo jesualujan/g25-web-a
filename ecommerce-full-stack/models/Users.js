36d5025a4f0e01a2829c874d9196fc870beb9995
jesualujan <lujan.oficial@gmail.com>
2024-02-07 21:19:29 -0600

    ✨ creamos nuestro schema de usuarios

---
+++ ecommerce-full-stack/models/Users.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema (
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        isAdmin: {type: Boolean, required, default: true},
+       },
+       { timestamps: true } // sirve para saber cuando un producto (dato) fue creado
+)
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+export default User
