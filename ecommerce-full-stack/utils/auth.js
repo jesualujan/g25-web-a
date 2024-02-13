b1bdc452f26ab5c5e890fa02a69fc5f0b0350425
jesualujan <lujan.oficial@gmail.com>
2024-02-12 20:14:55 -0600

    ✨hasheamos contraseña

---
+++ ecommerce-full-stack/utils/auth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+import {hash} from 'bcryptjs'
+
+export const hashPassword = async (password) => {
+    if(password.length < 8) {
+        throw new Error('Password must be at least 8 characters')
+    }
+    return await hash(password, 12)
+ }
+
+ export const signToken = (user) => {
+    return jwt.sign({ 
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    },
+        process.env.JWT_SECRET, //secret key
+    )
+ }
