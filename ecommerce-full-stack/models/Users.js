f6d97793526e5fce1aa41df686a5050db2bbbb74
jesualujan <lujan.oficial@gmail.com>
2024-02-12 20:15:11 -0600

    🐛 fixed bug

--- ecommerce-full-stack/models/Users.js
+++ ecommerce-full-stack/models/Users.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema (
         name: {type: String, required: true},
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true},
-        isAdmin: {type: Boolean, required, default: true},
+        isAdmin: {type: Boolean, required:false, default: true},
        },
        { timestamps: true } // sirve para saber cuando un producto (dato) fue creado
 )
