f8c24299133f306e3db7366867b0fb9beb445364
jesualujan <lujan.oficial@gmail.com>
2024-02-12 20:15:56 -0600

    ✨ auth para login

---
+++ ecommerce-full-stack/pages/api/users/login.js
@@ -0,0 +1,39 @@
+import { createRouter } from 'next-connect'
+import db from '../../utils/db'
+import User from '../../../models/Users'
+import bcrypt from 'bcryptjs'
+import {signToken} from '../../../utils/auth'
+
+
+const router = createRouter();
+
+router
+  .post(async (req, res) => { 
+    await db.connect()
+  const user = await User.findOne({email: req.body.email})
+    await db.disconnect()
+    // vamos a ver lo que el usuario nos retorna
+    if(user && bcrypt.compareSync(req.body.password, user.password)){
+        const token = signToken(user)
+        res.send({ 
+            token,
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin
+        })
+    }else {
+        res.statusCode(401).send({message: 'Invalid email or password'})
+    }
+  })
+
+
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).end(err.message);
+  },
+  onNoMatch: (req,res) => {
+    res.status(404).end('Page is not found');
+  }
+});
