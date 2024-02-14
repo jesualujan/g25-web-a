d746496c73608fe39400359a2ca7150787c7d57a
jesualujan <lujan.oficial@gmail.com>
2024-02-14 12:57:22 -0600

    ✨ importamos next-auth y session provider

--- ecommerce-full-stack/pages/_app.js
+++ ecommerce-full-stack/pages/_app.js
@@ -2,15 +2,22 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 import { CartProvider } from "../context/CartContext";
+import { SessionProvider } from "next-auth/react";
+import { wrapper } from "../redux/store";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps:{ session, ...pageProps} }) {
+  const {store, props} = wrapper.useWrappedStore(pageProps)
   return (
     <ChakraProvider>
+      <SessionProvider session={session}>
       <CartProvider>
+        <Provider store={store}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        </Provider>
       </CartProvider>
+      </SessionProvider>
     </ChakraProvider>
   );
 }
