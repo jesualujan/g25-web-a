d8a1aab3d1fd009beca6a5a031113cfec84969db
jesualujan <lujan.oficial@gmail.com>
2024-02-14 14:54:20 -0600

    ✨ modificamos el import

--- ecommerce-full-stack/pages/api/users/login.js
+++ ecommerce-full-stack/pages/api/users/login.js
@@ -1,5 +1,5 @@
 import { createRouter } from 'next-connect'
-import db from '../../utils/db'
+import db from '../../../utils/db'
 import User from '../../../models/Users'
 import bcrypt from 'bcryptjs'
 import {signToken} from '../../../utils/auth'
