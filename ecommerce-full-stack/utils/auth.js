ffd74a4745b9bbdb81c243d5a9a051d1d8125280
jesualujan <lujan.oficial@gmail.com>
2024-02-14 13:36:18 -0600

    ✨ add comments

--- ecommerce-full-stack/utils/auth.js
+++ ecommerce-full-stack/utils/auth.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken'
 import {hash} from 'bcryptjs'
 
+//Encriptar password
 export const hashPassword = async (password) => {
     if(password.length < 8) {
         throw new Error('Password must be at least 8 characters')
