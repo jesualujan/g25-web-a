5a56e63bcc410f6ba6e8ceabe78ba91380dbe145
jesualujan <lujan.oficial@gmail.com>
2024-02-14 20:50:53 -0600

    ✨ agregamos al estado inicial PaymentMethod

--- ecommerce-full-stack/redux/reducer.js
+++ ecommerce-full-stack/redux/reducer.js
@@ -3,31 +3,34 @@
 
 //* HYDRATE es una acción especial en next-redux-wrapper que se utiliza para sincronizar el estado
 //* del servidor con el estado del cliente en una aplicación Next.js con Redux.
-import { HYDRATE } from 'next-redux-wrapper'
+import { HYDRATE } from "next-redux-wrapper";
 
 // creamos el estado inicial
 const initialState = {
-    currentStep: 0,
-    shippingAddress: {
-        fullName: '',
-        address: '',
-        city: '',
-        postalCode: '',
-        country: ''
-    }
-}
+  currentStep: 0,
+  shippingAddress: {
+    fullName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+  },
+  PaymentMethod: "",
+};
 
 export const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case HYDRATE:
-      return {...state, ...action.payload};
-    case 'address/saveShippingAddress':
-      return {...state, shippingAddress: action.payload};
-      case 'checkout/nextStep':
-        return {...state, currentStep: state.currentStep + 1};
-        case 'checkout/prevStep':
-            return {...state, currentStep: state.currentStep - 1};
+  switch (action.type) {
+    case HYDRATE:
+      return { ...state, ...action.payload };
+    case "address/saveShippingAddress":
+      return { ...state, shippingAddress: action.payload };
+    case "checkout/nextStep":
+      return { ...state, currentStep: state.currentStep + 1 };
+    case "checkout/prevStep":
+      return { ...state, currentStep: state.currentStep - 1 };
+    case "checkout/paymentMethod":
+      return {...state, paymentMethod: action.payload}
     default:
       return state;
-    }
-}
+  }
+};
