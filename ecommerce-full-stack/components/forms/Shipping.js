c60c9f67c63b4226d5b7ef913b50bf971d307ff7
jesualujan <lujan.oficial@gmail.com>
2024-02-14 20:23:50 -0600

    ✨ agregamos import de dispatch y useSelector

--- ecommerce-full-stack/components/forms/Shipping.js
+++ ecommerce-full-stack/components/forms/Shipping.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     FormControl,
     FormLabel,
@@ -6,8 +6,11 @@ import {
     Select,
     Flex,
     Button,
+    FormHelperText
   } from "@chakra-ui/react";
   import * as Yup from 'yup'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
   
   const shippingAddressSchema = Yup.object().shape({
     fullName: Yup.string().required('Full Name is required'),
@@ -18,39 +21,80 @@ import {
   })
 
 
-
-
-
 const ShippingAddressForm = () => {
+  // el hook useDispatch en react redux y sirve para enviar acciones a la store de redux directamente
+  // desde un componente de React
+  const dispatch = useDispatch()
+  const address = useSelector((state) => state.shippingAddress)
+
+  const [formValues, setFormValues] = useState ( address ? address : {
+    fullName: '',
+    address: '',
+    city: '',
+    postalCode: '',
+    country: '',
+  })
+  const [error, setError] = useState({})
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormValues ({...formValues, [name]: value})
+  }
+
+  const handleSubmit = async (event) => {
+     event.preventDefault()
+
+      //validate form
+      try {
+        await shippingAddressSchema.validate(formValues,{
+            abortEarly: false
+        })
+    }catch(err){
+        const validationError = {}
+        if(err instanceof Yup.ValidationError){
+            err.inner.forEach(({path, message}) => {
+                validationError[path] = message
+            })
+        }
+            setError(validationError)
+            return
+    }
+
+     // valido el formulario
+     dispatch ({type: 'address/saveShippingAddress', payload: formValues})
+     // redireccionar al siguiente formulario
+  }
+
   return (
-    <form >
+    <form onSubmit={handleSubmit}>
         <FormControl>
             <FormLabel htmlFor='fullName'>Full Name</FormLabel>
-            <Input placeholder='Jesua Lujan' name='fullName'>Full Name</Input>
+            <Input placeholder='Jesua Lujan' name='fullName' value={formValues.fullName} onChange={handleChange}>Full Name</Input>
         </FormControl>
 
         <FormControl>
             <FormLabel htmlFor='address'>Address</FormLabel>
-            <Input placeholder='Mexico City' name='address'></Input>
+            <Input placeholder='Mexico City' name='address' value={formValues.address} onChange={handleChange}></Input>
         </FormControl>
 
         <FormControl>
             <FormLabel htmlFor='city'>City</FormLabel>
-            <Input placeholder='Any town' name='city'></Input>
+            <Input placeholder='Any town' name='city' value={formValues.city} onChange={handleChange}></Input>
         </FormControl>
 
         <FormControl>
             <FormLabel htmlFor='postalCode'>Postal Code</FormLabel>
-            <Input placeholder='1234' name='postalCode'></Input>
+            <Input placeholder='1234' name='postalCode' value={formValues.postalCode} onChange={handleChange}></Input>
         </FormControl>
 
         <FormControl>
             <FormLabel htmlFor='country'>Country</FormLabel>
-            <Select placeholder='Select a country' name='country'>
+            <Select placeholder='Select a country' name='country' value={formValues.country}  onChange={handleChange}>
             <option value="us">United State</option>
             <option value="mx">Mexico</option>
             <option value="ca">Canada</option>
             </Select>
+            
         </FormControl>
 
         <Flex display="flex" justify="flex-end">
