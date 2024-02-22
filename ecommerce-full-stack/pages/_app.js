ddec553fc7c90819eea6966b7126f05d3921714c
jesualujan <lujan.oficial@gmail.com>
2024-02-21 19:02:10 -0600

    ✨ importamos el proveedor de paypal

--- ecommerce-full-stack/pages/_app.js
+++ ecommerce-full-stack/pages/_app.js
@@ -5,12 +5,14 @@ import { CartProvider } from "../context/CartContext";
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { wrapper } from "../redux/store";
+import { PaypalScriptProvider } from "@paypal/react-paypal-js";
 
 export default function App({ Component, pageProps:{ session, ...pageProps} }) {
   const {store, props} = wrapper.useWrappedStore(pageProps)
   return (
     <ChakraProvider>
       <SessionProvider session={session}>
+      <PaypalScriptProvider deferLoaing={true}>
       <CartProvider>
         <Provider store={store}>
         <Layout>
@@ -18,6 +20,7 @@ export default function App({ Component, pageProps:{ session, ...pageProps} }) {
         </Layout>
         </Provider>
       </CartProvider>
+      </PaypalScriptProvider>
       </SessionProvider>
     </ChakraProvider>
   );
